09fbb9c8ec2c0dc4a614f8618b19d65a09292d12
zudww <zudwduz@gmail.com>
2026-07-13 01:50:29 +0800

    feat(attrset): add `lib.filterAttrs`

--- src/attrset.nix
+++ src/attrset.nix
@@ -7,6 +7,14 @@ lib: let
 
 in {
 
+  filterAttrs = attrset: shouldKeep:
+    foldl' (
+      acc: attr:
+        if shouldKeep attr attrset.${attr} then acc
+        else removeAttrs acc [ attr ]
+    )
+    attrset (attrNames attrset);
+
   remapAttrs = attrset: remapAttr:
     foldl' (
       acc: attr:
--- src/default.nix
+++ src/default.nix
@@ -8,6 +8,7 @@ let
 
   toplevel = {
     inherit (sublibs.attrset)
+      filterAttrs
       remapAttrs
       ;
 
