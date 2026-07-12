b0c2e8bf1e4425b975c8cb42fab3b64ab3c095f6
zudww <zudwduz@gmail.com>
2026-07-13 01:11:20 +0800

    feat(types): add `lib.isType`

--- src/default.nix
+++ src/default.nix
@@ -16,6 +16,7 @@ let
       ;
 
     inherit (sublibs.types)
+      isType
       typesOf
       ;
   };
--- src/types.nix
+++ src/types.nix
@@ -7,6 +7,8 @@ lib: let
 
 in {
 
+  isType = type: val: (typeOf val) == type;
+
   typesOf = attrs: mapAttrs (_: val: typeOf val) attrs;
 
 }
