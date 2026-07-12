82103ef6885db1564d1815867e54b320917c3da2
zudww <zudwduz@gmail.com>
2026-07-13 02:52:39 +0800

    feat(list): add `lib.tailElems`

--- src/default.nix
+++ src/default.nix
@@ -35,6 +35,7 @@ let
 
     inherit (sublibs.list)
       headElem
+      tailElems
       ;
   };
 
--- src/list.nix
+++ src/list.nix
@@ -2,10 +2,15 @@ lib: let
 
   inherit (builtins)
     elemAt
+    genList
+    length
     ;
 
 in {
 
   headElem = list: elemAt list 0;
+
+  tailElems = list:
+    genList (i: elemAt list (i + 1)) ((length list) - 1);
 
 }
